6f0285dc2a6088318ea75211acfa11964d253666
unknown <nikajibladze5@gmail.com>
2022-04-19 15:18:17 +0400

    First commit

---
+++ app/Http/Controllers/SendController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SendController extends Controller
+{
+    // Here is the responsible controller of sending an email on gmail
+}
